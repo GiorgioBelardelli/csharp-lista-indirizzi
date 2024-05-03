a807654d40a0de8c1ade6242318d41d6f1064ef4
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-05-03 11:18:02 +0200

    Es. finito

---
+++ Indirizzo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Via { get; set; }
+        public string Citta { get; set; }
+        public string Provincia { get; set; }
+        public int CAP { get; set; }
+
+
+
+        public Indirizzo(string nome, string cognome, string via, string citta, string provincia, int cap)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+            this.Via = via;
+            this.Citta = citta;
+            this.Provincia = provincia;
+            this.CAP = cap;
+
+        }
+        public override string ToString()
+        {
+            return $"{Nome}, {Cognome}, {Via}, {Citta}, {Provincia}, {CAP} ";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:\\Users\\aster\\Desktop\\Esercizi .NET\\csharp-lista-indirizzi\\ListaIndirizzi.txt";
+
+            var indirizzi = LeggiDaTesto(path);
+            foreach (var indirizzo in indirizzi)
+                Console.WriteLine(indirizzo.ToString());
+        }
+        public static List<Indirizzo> LeggiDaTesto(string path)
+        {
+            
+            List<Indirizzo> ListaIndirizzi = new List<Indirizzo>();
+            var stream = File.OpenText(path);
+            int i = 0;
+
+            while (stream.EndOfStream == false)
+            {
+                var linea = stream.ReadLine();
+                i++;
+                if (i < 2)
+                {
+                    continue;
+                }
+                try
+                {
+                    var dati = linea.Split(',');
+                    string Nome = dati[0];
+                    string Cognome = dati[1];
+                    string Via = dati[2];
+                    string Citta = dati[3];
+                    string Provincia = dati[4];
+                    int CAP = int.Parse(dati[5]);
+
+                    Indirizzo dato = new Indirizzo(Nome, Cognome, Via, Citta, Provincia, CAP);
+                    ListaIndirizzi.Add(dato);
+                }
+                catch (Exception e)
+                {
+                    string errorMessage = $"Errore di dati nella riga {i}: I dati letti dal file non sono validi o mancanti.";
+                    Console.WriteLine(errorMessage);
+                }
+            }
+            stream.Dispose();
+            return ListaIndirizzi;
         }
     }
 }
